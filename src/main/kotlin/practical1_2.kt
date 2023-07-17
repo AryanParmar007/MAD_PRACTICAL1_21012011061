b50871e1c3fc494120edc8aa6579555b0c32b392
AryanParmar007 <aryangxly007@gmail.com>
2023-07-17 10:21:45 +0530

    sample code 3

---
+++ src/main/kotlin/practical1_2.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val a:Int=22;
+    println("Integer value: $a");
+    val b:Double= a.toDouble();
+    println("Double value (From Integer): $b");
+    val c:String= "10";
+    println("String value: $c");
+//    val d:I = c.toString();
+//    println("Integer value1 (From String): $d");
+}
