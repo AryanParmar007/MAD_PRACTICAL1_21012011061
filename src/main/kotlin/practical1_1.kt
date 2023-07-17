85b05644717266924d0a44a9dacc6cb57374a9a5
AryanParmar007 <aryangxly007@gmail.com>
2023-07-17 10:21:45 +0530

    sample code 1

---
+++ src/main/kotlin/practical1_1.kt
@@ -0,0 +1,21 @@
+fun main(){
+    val a:Int=22;
+    println("Integer value: $a");
+    val b :Float= 1.5F;
+    println("Float value: $b");
+    val c :Char='T';
+    println("Character value: $c");
+    val d :String= "Aryan";
+    println("String value: $d");
+    val e :Boolean= false;
+    println("Boolean value: $e");
+    val f :Double= 96.56;
+    println("Double value: $f");
+    val g :Long= 124578516;
+    println("Long  value: $g");
+    val h :Short= -2;
+    println("Short value: $h");
+    val i :Byte= 127;
+    println("Float value: $i");
+
+}
