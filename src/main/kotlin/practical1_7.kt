91793e7da2720fe2edbc93a107113c7f15ccc0b8
AryanParmar007 <124689058+AryanParmar007@users.noreply.github.com>
2023-07-23 21:44:13 +0530

    Merge remote-tracking branch 'origin/master'

---
+++ src/main/kotlin/practical1_7.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val num = 4
+    val result: Long
+    result = factorial(num)
+    println("Factorial of $num is = $result")
+}
+
+fun factorial(n: Int): Long {
+    return if(n == 1){
+        n.toLong()
+    }
+    else{
+        n*factorial(n-1)
+    }
+}
